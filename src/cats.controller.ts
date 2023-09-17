397b92875976392d44b81613f13985c3e1264303
waffe_x20 <argueta_eduardo@yahoo.de>
2023-09-16 21:51:29 -0600

    Add basic Post handler

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(@Body() createCatDto: CreateCatDto): string {
+    return 'This action adds a new cat';
+  }
+
   @Get()
   findAll(): string {
     return 'This action REturns all gatillos!';
